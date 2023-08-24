36a385e2207b002dcfadf17f71720f7708a6afb5
Filip Tovaryš <tovarys.filip@gmail.com>
2023-08-24 16:56:01 +0200

    hamburger bars on-scroll color change WORKS

--- script.js
+++ script.js
@@ -6,12 +6,14 @@ const logo = document.getElementById("fajntajm_logo");
 const ig = document.getElementById("ig_header");
 const fb = document.getElementById("fb_header");
 const hamburger = document.querySelector(".fill_empty_bar");
-const bars = document.getElementById("top_bar");
+const topBar = document.getElementById("top_bar");
+const middleBar = document.getElementById("middle_bar");
+const bottomBar = document.getElementById("bottom_bar");
 const navbar = document.querySelector(".mid_section");
 const navbar_path = document.getElementById("bars_white");
 const right_section = document.getElementsByClassName("right_section")[0];
 const body = document.getElementsByTagName("body")[0];
-console.log(bars);
+
 
 
 window.onscroll = function() {
@@ -23,7 +25,9 @@ window.onscroll = function() {
     languages.classList.add("languages_color_change");
     ig.classList.add("ig_logo_color_change");
     fb.classList.add("fb_logo_color_change");
-    bars.classList.add("bars_color_change");
+    topBar.classList.add("bars_color_change");
+    middleBar.classList.add("bars_color_change");
+    bottomBar.classList.add("bars_color_change");
     console.log("added");
   } else {
     logo.classList.remove("logo_color_change");
@@ -32,7 +36,9 @@ window.onscroll = function() {
     languages.classList.remove("languages_color_change");
     ig.classList.remove("ig_logo_color_change");
     fb.classList.remove("fb_logo_color_change");
-    bars.classList.remove("bars_color_change");
+    topBar.classList.remove("bars_color_change");
+    middleBar.classList.remove("bars_color_change");
+    bottomBar.classList.remove("bars_color_change");
   }
 };
 
